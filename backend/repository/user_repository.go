dfa09d8556f794a0db87e5274a3076dc003315c6
seus31 <us.balut.big.dipper@gmail.com>
2024-07-16 23:44:03 +0900

    add: users一覧のGetUsersメソッドに関するrepository interface及びrepositoryの作成

--- backend/repository/user_repository.go
+++ backend/repository/user_repository.go
@@ -15,6 +15,14 @@ func NewUserRepository(db *gorm.DB) interfaces.UserRepositoryInterface {
 	return &UserRepository{db: db}
 }
 
+func (r *UserRepository) GetUsers(ctx context.Context, limit int, offset int) ([]*models.User, error) {
+	var users []*models.User
+	if err := r.db.Limit(limit).Offset(offset).Find(&users).Error; err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
 func (r *UserRepository) Create(ctx context.Context, user *models.User) error {
 	return r.db.WithContext(ctx).Create(user).Error
 }
