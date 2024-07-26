660e854f843e18cf746510b4d22be261e9adfe0f
seus31 <us.balut.big.dipper@gmail.com>
2024-07-27 00:45:03 +0900

    add: ユーザー登録のrequestクラスとレスポンスクラスを追加

---
+++ backend/dto/responses/get_user_response.go
@@ -0,0 +1,9 @@
+package responses
+
+type UserResponse struct {
+	ID        uint   `json:"id"`
+	Name      string `json:"name"`
+	Email     string `json:"email"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+}
