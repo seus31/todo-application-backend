86ee81d9147c33f242aed61e2b37985ee6c8518f
seus31 <us.balut.big.dipper@gmail.com>
2024-09-27 21:57:19 +0900

    カテゴリ削除APIのための削除処理のrepository interfaceを定義

--- backend/interfaces/category_repository_interface.go
+++ backend/interfaces/category_repository_interface.go
@@ -10,4 +10,5 @@ type CategoryRepositoryInterface interface {
 	GetCategories(ctx context.Context, limit int, offset int) ([]*models.Category, error)
 	GetCategoryByID(ctx context.Context, id uint) (*models.Category, error)
 	Update(ctx context.Context, category *models.Category) error
+	Delete(ctx context.Context, category *models.Category) error
 }
