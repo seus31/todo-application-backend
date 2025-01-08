d0d0a22ecc207ca8aade1d2cea2cd183b68953c5
seus31 <us.balut.big.dipper@gmail.com>
2025-01-08 18:22:23 +0900

    category controllerの各種バリデーションの修正

--- app/controllers/category_controller.go
+++ app/controllers/category_controller.go
@@ -2,11 +2,11 @@ package controllers
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/seus31/todo-application/backend/dto/requests/categories"
-	"github.com/seus31/todo-application/backend/dto/responses"
-	"github.com/seus31/todo-application/backend/models"
-	"github.com/seus31/todo-application/backend/services"
-	"github.com/seus31/todo-application/backend/utils"
+	"github.com/seus31/todo-application-backend/dto/requests/categories"
+	"github.com/seus31/todo-application-backend/dto/responses"
+	"github.com/seus31/todo-application-backend/models"
+	"github.com/seus31/todo-application-backend/services"
+	"github.com/seus31/todo-application-backend/utils"
 	"log"
 	"strconv"
 	"time"
@@ -28,7 +28,8 @@ func (cc *CategoryController) CreateCategory(ctx *fiber.Ctx) error {
 		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Request parsing failed"})
 	}
 
-	if err := utils.ValidateStruct(&req); err != nil {
+	validate := categories.CreateCategoryRequestValidator()
+	if err := utils.ValidateStruct(validate, &req); err != nil {
 		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
 	}
 
@@ -55,7 +56,8 @@ func (cc *CategoryController) GetCategories(ctx *fiber.Ctx) error {
 		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid parameters"})
 	}
 
-	if err := utils.ValidateStruct(&req); err != nil {
+	validate := categories.GetCategoriesRequestValidator()
+	if err := utils.ValidateStruct(validate, &req); err != nil {
 		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
 	}
 
@@ -75,7 +77,8 @@ func (cc *CategoryController) GetCategory(ctx *fiber.Ctx) error {
 		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid category ID"})
 	}
 
-	if err := utils.ValidateStruct(req); err != nil {
+	validate := categories.GetCategoryRequestValidator()
+	if err := utils.ValidateStruct(validate, req); err != nil {
 		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
 	}
 
@@ -104,7 +107,8 @@ func (cc *CategoryController) UpdateCategory(ctx *fiber.Ctx) error {
 		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Cannot parse JSON"})
 	}
 
-	if err := utils.ValidateStruct(req); err != nil {
+	validate := categories.UpdateCategoryRequestValidator()
+	if err := utils.ValidateStruct(validate, req); err != nil {
 		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
 	}
 
