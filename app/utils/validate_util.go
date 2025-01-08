043baecd10193d2c4fb2f159bc4ef965f80959dc
seus31 <us.balut.big.dipper@gmail.com>
2025-01-08 18:20:09 +0900

    バリデーションのユーティリティメソッド修正

--- app/utils/validate_util.go
+++ app/utils/validate_util.go
@@ -4,8 +4,6 @@ import (
 	"github.com/go-playground/validator/v10"
 )
 
-var validate = validator.New()
-
-func ValidateStruct(s interface{}) error {
+func ValidateStruct(validate *validator.Validate, s interface{}) error {
 	return validate.Struct(s)
 }
