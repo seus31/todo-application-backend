571f57c773392e483182f6e802b4e98c49a795c3
seus31 <us.balut.big.dipper@gmail.com>
2025-01-08 18:20:44 +0900

    tasksテーブルにカラム追加したことによるタスク作成リクエストのバリデーション修正

--- app/dto/requests/tasks/create_task_request.go
+++ app/dto/requests/tasks/create_task_request.go
@@ -1,5 +1,30 @@
 package tasks
 
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/seus31/todo-application-backend/models"
+)
+
 type CreateTaskRequest struct {
-	TaskName string `json:"task_name" validate:"required,max=255"`
+	TaskName string            `json:"task_name" validate:"required,max=255"`
+	UserID   uint              `json:"user_id" validate:"required,numeric"`
+	ParentID *uint             `json:"parent_id" validate:"omitempty,numeric"`
+	DueDate  *string           `json:"due_date" validate:"omitempty,datetime=2006-01-02"`
+	DueTime  *string           `json:"due_time" validate:"omitempty,datetime=15:04"`
+	Status   models.TaskStatus `json:"status" validate:"required,valid_task_status"`
+	Priority models.Priority   `json:"priority" validate:"required,valid_priority"`
+}
+
+var createTaskRequestValidate *validator.Validate
+
+func init() {
+	createTaskRequestValidate = models.GetTaskValidator()
+}
+
+func CreateTaskRequestValidator() *validator.Validate {
+	return createTaskRequestValidate
+}
+
+func (r *CreateTaskRequest) Validate() error {
+	return createTaskRequestValidate.Struct(r)
 }
