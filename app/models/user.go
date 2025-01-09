fabc63b8bf5965de48c23c52d742c4220de75716
seus31 <us.balut.big.dipper@gmail.com>
2025-01-09 13:42:49 +0900

    user構造体の修正

--- app/models/user.go
+++ app/models/user.go
@@ -6,7 +6,7 @@ import (
 
 type User struct {
 	gorm.Model
-	Name     string `validate:"required,max=255"`
-	Email    string `validate:"required,email"`
-	Password string `validate:"required,min=8,max=64,containsany=!@#$%^&*()_+-=[]{};:,"`
+	Name     string `json:"name" validate:"required,max=255"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"-" validate:"required,min=8,max=64,containsany=!@#$%^&*()_+-=[]{};:,"`
 }
