d8e5b26363fefca67c83105236475a21c05ecb29
seus31 <us.balut.big.dipper@gmail.com>
2025-02-01 23:49:52 +0900

    usersテーブルのnameとemailをuniqueに修正

--- app/database/migrations/000002_create_users_table.up.sql
+++ app/database/migrations/000002_create_users_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
     id SERIAL NOT NULL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NULL,
     updated_at TIMESTAMP NULL,
