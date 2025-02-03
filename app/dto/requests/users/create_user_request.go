d79cc68029b133ca237f37c6b82aac6fbab446e5
seus31 <us.balut.big.dipper@gmail.com>
2025-02-03 14:42:05 +0900

    create_user_requestの構造体修正

--- app/dto/requests/users/create_user_request.go
+++ app/dto/requests/users/create_user_request.go
@@ -3,9 +3,10 @@ package users
 import "github.com/go-playground/validator/v10"
 
 type CreateUserRequest struct {
-	Name     string `json:"name" validate:"required,max=255"`
-	Email    string `json:"email" validate:"required,email,max=255"`
-	Password string `json:"password" validate:"required,min=8,max=64,containsany=!@#$%^&*()_+-=[]{};:"`
+	Name            string `json:"name" validate:"required,max=255"`
+	Email           string `json:"email" validate:"required,email,max=255"`
+	Password        string `json:"password" validate:"required,min=8,max=64,containsany=!@#$%^&*()_+-=[]{};:"`
+	ConfirmPassword string `json:"confirm_password" validate:"required"`
 }
 
 var createUserRequestValidate *validator.Validate
