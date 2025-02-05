226cc63f59e8c796c5542e9424acdf2c88469296
seus31 <us.balut.big.dipper@gmail.com>
2025-02-05 18:54:46 +0900

    auth_middlewareを修正

--- app/middleware/auth_middleware.go
+++ app/middleware/auth_middleware.go
@@ -1,14 +1,54 @@
 package middleware
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gofiber/fiber/v2"
+	"github.com/seus31/todo-application-backend/config"
+)
 
 func AuthMiddleware(c *fiber.Ctx) error {
+	secretKey := config.Config("SECRET_KEY")
 	userToken := c.Get("X-User-Token")
+
 	if userToken == "" {
 		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
 			"error": "Unauthorized: User Token not provided",
 		})
 	}
+
+	token, err := jwt.Parse(userToken, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, fmt.Errorf("unexpected signing method")
+		}
+		return []byte(secretKey), nil
+	})
+
+	if err != nil || !token.Valid {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": "Unauthorized: Invalid Token",
+		})
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": "Unauthorized: Invalid Token Claims",
+		})
+	}
+
+	idValue, ok := claims["id"].(float64)
+	if !ok {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": "Unauthorized: Invalid User ID",
+		})
+	}
+	userID := uint(idValue)
+
 	c.Locals("userToken", userToken)
+	c.Locals("userName", claims["name"])
+	c.Locals("userID", userID)
+
 	return c.Next()
 }
