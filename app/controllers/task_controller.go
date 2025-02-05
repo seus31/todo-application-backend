fba1bae2e0f5cbe9ef42e78d0319f0e26b078b5e
seus31 <us.balut.big.dipper@gmail.com>
2025-02-05 18:58:49 +0900

    task_controllerからtask_serviceへロジックを移行

--- app/controllers/task_controller.go
+++ app/controllers/task_controller.go
@@ -1,15 +1,9 @@
 package controllers
 
 import (
+	"errors"
 	"github.com/gofiber/fiber/v2"
-	"github.com/seus31/todo-application-backend/dto/requests/tasks"
-	"github.com/seus31/todo-application-backend/dto/responses"
-	"github.com/seus31/todo-application-backend/models"
 	"github.com/seus31/todo-application-backend/services"
-	"github.com/seus31/todo-application-backend/utils"
-	"log"
-	"strconv"
-	"time"
 )
 
 type TaskController struct {
@@ -23,128 +17,71 @@ func NewTaskController(taskService *services.TaskService) *TaskController {
 }
 
 func (tc *TaskController) GetTasks(ctx *fiber.Ctx) error {
-	var req tasks.GetTasksRequest
-	if err := ctx.BodyParser(&req); err != nil {
-		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid parameters"})
-	}
-
-	validate := tasks.GetTasksRequestValidator()
-	if err := utils.ValidateStruct(validate, &req); err != nil {
-		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
-	}
-
-	offset := (req.Page - 1) * req.Limit
-	tasksData, err := tc.TaskService.GetTasks(utils.GetContextFromFiber(ctx), req.Limit, offset)
+	tasksData, err := tc.TaskService.GetTasks(ctx)
 	if err != nil {
-		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to fetch users"})
+		if errors.Is(err, services.ErrUnexpectedError) {
+			return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+		} else {
+			return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+		}
 	}
 
 	return ctx.JSON(tasksData)
 }
 
 func (tc *TaskController) CreateTask(ctx *fiber.Ctx) error {
-	var req tasks.CreateTaskRequest
-	if err := ctx.BodyParser(&req); err != nil {
-		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
-	}
-
-	validate := tasks.CreateTaskRequestValidator()
-	if err := utils.ValidateStruct(validate, &req); err != nil {
-		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
-	}
-
-	task := &models.Task{
-		TaskName: req.TaskName,
-		UserID:   req.UserID,
-		ParentID: req.ParentID,
-		DueDate:  req.DueDate,
-		DueTime:  req.DueTime,
-		Status:   req.Status,
-		Priority: req.Priority,
-	}
-
-	if err := tc.TaskService.CreateTask(utils.GetContextFromFiber(ctx), task); err != nil {
-		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to create task"})
+	task, err := tc.TaskService.CreateTask(ctx)
+	if err != nil {
+		if !errors.Is(err, services.ErrUnexpectedError) {
+			return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+		} else {
+			return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+		}
 	}
 
 	return ctx.Status(fiber.StatusCreated).JSON(task)
 }
 
 func (tc *TaskController) GetTask(ctx *fiber.Ctx) error {
-	var req tasks.GetTaskRequest
-	if err := ctx.ParamsParser(&req); err != nil {
-		log.Print(err)
-		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid task ID"})
-	}
-
-	validate := tasks.GetTaskRequestValidator()
-	if err := utils.ValidateStruct(validate, req); err != nil {
-		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
-	}
-
-	task, err := tc.TaskService.GetTask(utils.GetContextFromFiber(ctx), uint(req.ID))
+	task, err := tc.TaskService.GetTask(ctx)
 	if err != nil {
-		return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Task not found"})
+		if !errors.Is(err, services.ErrUnexpectedError) {
+			return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+		} else if !errors.Is(err, services.ErrRecordNotFound) {
+			return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": err.Error()})
+		} else {
+			return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+		}
 	}
 
-	response := responses.TaskResponse{
-		ID:        task.ID,
-		TaskName:  task.TaskName,
-		CreatedAt: task.CreatedAt.Format(time.RFC3339),
-	}
-
-	return ctx.Status(fiber.StatusOK).JSON(response)
+	return ctx.Status(fiber.StatusOK).JSON(&task)
 }
 
 func (tc *TaskController) UpdateTask(ctx *fiber.Ctx) error {
-	var req tasks.UpdateTaskRequest
-	taskId, err := strconv.Atoi(ctx.Params("id"))
+	task, err := tc.TaskService.UpdateTask(ctx)
 	if err != nil {
-		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid task ID"})
+		if !errors.Is(err, services.ErrUnexpectedError) {
+			return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+		} else if !errors.Is(err, services.ErrRecordNotFound) {
+			return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": err.Error()})
+		} else {
+			return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+		}
 	}
 
-	if err := ctx.BodyParser(&req); err != nil {
-		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Cannot parse JSON"})
-	}
-
-	validate := tasks.UpdateTaskRequestValidator()
-	if err := utils.ValidateStruct(validate, req); err != nil {
-		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
-	}
-
-	task, err := tc.TaskService.GetTask(utils.GetContextFromFiber(ctx), uint(taskId))
-	if err != nil {
-		return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Task not found"})
-	}
-
-	updatedTask, err := tc.TaskService.UpdateTask(utils.GetContextFromFiber(ctx), task, req)
-	if err != nil {
-		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Cannot update task"})
-	}
-
-	response := responses.TaskResponse{
-		ID:        updatedTask.ID,
-		TaskName:  updatedTask.TaskName,
-		CreatedAt: updatedTask.CreatedAt.Format(time.RFC3339),
-		UpdatedAt: updatedTask.UpdatedAt.Format(time.RFC3339),
-	}
-
-	return ctx.Status(fiber.StatusOK).JSON(response)
+	return ctx.Status(fiber.StatusOK).JSON(&task)
 }
 
 func (tc *TaskController) DeleteTask(ctx *fiber.Ctx) error {
-	taskId, err := strconv.Atoi(ctx.Params("id"))
+	err := tc.TaskService.DeleteTask(ctx)
 	if err != nil {
-		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid task ID"})
-	}
-
-	task, err := tc.TaskService.GetTask(utils.GetContextFromFiber(ctx), uint(taskId))
-	if err != nil {
-		return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Task not found"})
-	}
-
-	if err := tc.TaskService.DeleteTask(utils.GetContextFromFiber(ctx), task); err != nil {
-		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Cannot delete task"})
+		if !errors.Is(err, services.ErrUnexpectedError) {
+			return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+		} else if !errors.Is(err, services.ErrRecordNotFound) {
+			return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": err.Error()})
+		} else {
+			return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+		}
 	}
 
 	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{"status": "OK"})
--- app/services/task_service.go
+++ app/services/task_service.go
@@ -1,10 +1,17 @@
 package services
 
 import (
-	"context"
+	"errors"
+	"fmt"
+	"github.com/gofiber/fiber/v2"
 	"github.com/seus31/todo-application-backend/dto/requests/tasks"
+	"github.com/seus31/todo-application-backend/dto/responses"
 	"github.com/seus31/todo-application-backend/interfaces"
 	"github.com/seus31/todo-application-backend/models"
+	"github.com/seus31/todo-application-backend/utils"
+	"gorm.io/gorm"
+	"strconv"
+	"time"
 )
 
 type TaskService struct {
@@ -17,30 +24,165 @@ func NewTaskService(repo interfaces.TaskRepositoryInterface) *TaskService {
 	}
 }
 
-func (s *TaskService) GetTasks(ctx context.Context, limit int, offset int) ([]*models.Task, error) {
-	return s.taskRepo.GetTasks(ctx, limit, offset)
-}
+func (s *TaskService) GetTasks(ctx *fiber.Ctx) ([]*models.Task, error) {
+	var req tasks.GetTasksRequest
+	contextData := utils.GetContextFromFiber(ctx)
 
-func (s *TaskService) CreateTask(ctx context.Context, task *models.Task) error {
-	return s.taskRepo.Create(ctx, task)
-}
+	if err := ctx.BodyParser(&req); err != nil {
+		return nil, ErrFailedToParseRequest
+	}
 
-func (s *TaskService) GetTask(ctx context.Context, id uint) (*models.Task, error) {
-	return s.taskRepo.GetTaskByID(ctx, id)
-}
-
-func (s *TaskService) UpdateTask(ctx context.Context, task *models.Task, req tasks.UpdateTaskRequest) (*models.Task, error) {
-	task.TaskName = req.TaskName
-	err := s.taskRepo.Update(ctx, task)
-	if err != nil {
+	validate := tasks.GetTasksRequestValidator()
+	if err := utils.ValidateStruct(validate, &req); err != nil {
 		return nil, err
 	}
 
-	updateTask, err := s.taskRepo.GetTaskByID(ctx, task.ID)
+	offset := (req.Page - 1) * req.Limit
+	tasksData, err := s.taskRepo.GetTasks(contextData, req.Limit, offset)
 
-	return updateTask, err
+	if err != nil {
+		return nil, ErrUnexpectedError
+	}
+
+	return tasksData, nil
 }
 
-func (s *TaskService) DeleteTask(ctx context.Context, task *models.Task) error {
-	return s.taskRepo.Delete(ctx, task)
+func (s *TaskService) CreateTask(ctx *fiber.Ctx) (*models.Task, error) {
+	var req tasks.CreateTaskRequest
+	contextData := utils.GetContextFromFiber(ctx)
+
+	if err := ctx.BodyParser(&req); err != nil {
+		return nil, ErrFailedToParseRequest
+	}
+
+	validate := tasks.CreateTaskRequestValidator()
+	if err := utils.ValidateStruct(validate, &req); err != nil {
+		return nil, err
+	}
+
+	task := models.NewTask(
+		req.TaskName,
+		ctx.Locals("userID").(uint),
+		models.WithParentID(req.ParentID),
+		models.WithDueDate(*req.DueDate),
+		models.WithDueTime(*req.DueTime),
+		models.WithStatus(req.Status),
+		models.WithPriority(req.Priority),
+	)
+
+	if err := s.taskRepo.Create(contextData, task); err != nil {
+		return nil, ErrUnexpectedError
+	}
+
+	return task, nil
+}
+
+func (s *TaskService) GetTask(ctx *fiber.Ctx) (*responses.TaskResponse, error) {
+	var req tasks.GetTaskRequest
+	contextData := utils.GetContextFromFiber(ctx)
+
+	if err := ctx.ParamsParser(&req); err != nil {
+		return nil, ErrInvalidTaskID
+	}
+
+	validate := tasks.GetTaskRequestValidator()
+	if err := utils.ValidateStruct(validate, req); err != nil {
+		return nil, err
+	}
+
+	task, err := s.taskRepo.GetTaskByID(contextData, uint(req.ID))
+	if err != nil {
+		if !errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, ErrRecordNotFound
+		} else {
+			return nil, ErrUnexpectedError
+		}
+	}
+
+	response := &responses.TaskResponse{
+		ID:        task.ID,
+		TaskName:  task.TaskName,
+		CreatedAt: task.CreatedAt.Format(time.RFC3339),
+	}
+
+	return response, nil
+}
+
+func (s *TaskService) UpdateTask(ctx *fiber.Ctx) (*responses.TaskResponse, error) {
+	var req tasks.UpdateTaskRequest
+	contextData := utils.GetContextFromFiber(ctx)
+
+	taskId, err := strconv.Atoi(ctx.Params("id"))
+	if err != nil {
+		return nil, ErrInvalidTaskID
+	}
+
+	if err := ctx.BodyParser(&req); err != nil {
+		return nil, ErrInvalidParameters
+	}
+
+	validate := tasks.UpdateTaskRequestValidator()
+	if err := utils.ValidateStruct(validate, req); err != nil {
+		return nil, err
+	}
+
+	task, err := s.taskRepo.GetTaskByID(contextData, uint(taskId))
+	if err != nil {
+		if !errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, ErrRecordNotFound
+		} else {
+			return nil, ErrUnexpectedError
+		}
+	}
+
+	task.TaskName = req.TaskName
+	if req.ParentID == nil {
+		fmt.Println(req.ParentID)
+		task.ParentID = nil
+	} else {
+		task.ParentID = req.ParentID
+	}
+	task.DueDate = req.DueDate
+	task.DueTime = req.DueTime
+	task.Status = req.Status
+	task.Priority = req.Priority
+
+	err = s.taskRepo.Update(contextData, task)
+	if err != nil {
+		return nil, ErrUnexpectedError
+	}
+
+	response := &responses.TaskResponse{
+		ID:        task.ID,
+		TaskName:  task.TaskName,
+		CreatedAt: task.CreatedAt.Format(time.RFC3339),
+		UpdatedAt: task.UpdatedAt.Format(time.RFC3339),
+	}
+
+	return response, nil
+}
+
+func (s *TaskService) DeleteTask(ctx *fiber.Ctx) error {
+	contextData := utils.GetContextFromFiber(ctx)
+
+	taskId, err := strconv.Atoi(ctx.Params("id"))
+	if err != nil {
+		return ErrInvalidTaskID
+	}
+
+	task, err := s.taskRepo.GetTaskByID(contextData, uint(taskId))
+	if err != nil {
+		if !errors.Is(err, gorm.ErrRecordNotFound) {
+			return ErrRecordNotFound
+		} else {
+			return ErrUnexpectedError
+		}
+	}
+
+	err = s.taskRepo.Delete(contextData, task)
+	if err != nil {
+		return ErrUnexpectedError
+	}
+
+	return nil
 }
