2596ede95d9320392f8a9208577af81d61ab4dea
seus31 <us.balut.big.dipper@gmail.com>
2025-02-05 18:58:17 +0900

    service_errorにカスタムエラー追加

--- app/services/service_error.go
+++ app/services/service_error.go
@@ -11,4 +11,7 @@ var (
 	ErrFailedToHashPassword = errors.New("failed to hash password")
 	ErrUnexpectedError      = errors.New("unexpected error")
 	ErrInvalidCredentials   = errors.New("invalid credentials")
+	ErrInvalidParameters    = errors.New("invalid parameters")
+	ErrInvalidTaskID        = errors.New("invalid task id")
+	ErrRecordNotFound       = errors.New("record not found")
 )
