c151b8072356721ab4eb631078fe0ea4c16db39b
seus31 <us.balut.big.dipper@gmail.com>
2025-02-05 18:56:55 +0900

    update_task_request構造体を修正

--- app/dto/requests/tasks/update_task_request.go
+++ app/dto/requests/tasks/update_task_request.go
@@ -1,15 +1,23 @@
 package tasks
 
-import "github.com/go-playground/validator/v10"
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/seus31/todo-application-backend/models"
+)
 
 type UpdateTaskRequest struct {
-	TaskName string `json:"task_name" validate:"required,max=255"`
+	TaskName string            `json:"task_name" validate:"required,max=255"`
+	ParentID *uint             `json:"parent_id" validate:"omitempty,numeric"`
+	DueDate  *string           `json:"due_date" validate:"omitempty,datetime=2006-01-02"`
+	DueTime  *string           `json:"due_time" validate:"omitempty,datetime=15:04"`
+	Status   models.TaskStatus `json:"status" validate:"required,valid_task_status"`
+	Priority models.Priority   `json:"priority" validate:"required,valid_priority"`
 }
 
 var updateTaskRequestValidate *validator.Validate
 
 func init() {
-	updateTaskRequestValidate = validator.New()
+	updateTaskRequestValidate = models.GetTaskValidator()
 }
 
 func UpdateTaskRequestValidator() *validator.Validate {
